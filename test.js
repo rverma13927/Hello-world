01cca3ae490edbbb5c4afdbc62ed585c6f13b5bc
rverma13927 <65550578+rverma13927@users.noreply.github.com>
2020-05-21 20:25:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,26 +4,26 @@ determine total number of subset with sum
 equal to given sum.
 */
 function SubsetSum( array ,sum){
-     let dp =[]; // create an dp array where dp[i] denote number of subset with sum equal to i 
-     for (var i = 1; i <= sum; i++) {
-         dp[i] =0;
+     let dp =[] // create an dp array where dp[i] denote number of subset with sum equal to i 
+     for (let i = 1; i <= sum; i++) {
+         dp[i] =0
      }
-     dp[0]=1;  // since sum equal to 0 is always possible with no element in subset
+     dp[0]=1  // since sum equal to 0 is always possible with no element in subset
 
-     for (var i = 0; i < array.length; i++) {
-           for (var j = sum; j >= array[i]; j--) {
+     for (let i = 0; i < array.length; i++) {
+           for (let j = sum; j >= array[i]; j--) {
                  if( j-array[i] >= 0){
-                    dp[j] += dp[j-array[i]];
+                    dp[j] += dp[j-array[i]]
                  }
            }
      }
-     return dp[sum];
+     return dp[sum]
 }
 
 function main () {
-     var array=[1,1,1,1];
-     var sum=2;
-     var result = SubsetSum( array , sum);
-     console.log( result );
+     let array=[1,1,1,1]
+     let sum=2
+     let result = SubsetSum( array , sum)
+     console.log( result )
 }
-main();
+main()
